543d59a9f741724d59f69cd9c118dc598df33dd6
gustavo-fardo <gustavo.fardo@hotmail.com>
2025-03-06 13:17:05 +0000

    requisitos para selecao de classes e desenhar as imagens no anotador

--- AutoAnotador/annotator.py
+++ AutoAnotador/annotator.py
@@ -5,7 +5,9 @@ from pathlib import Path
 
 from ultralytics import YOLO
 
-def auto_annotate(data, det_model="yolov8x.pt", device="", output_dir=None):
+import cv2
+
+def auto_annotate(data, det_model="yolov8x.pt", device="", output_dir=None, desired_class_id=None, draw=False):
     """
     Anota automaticamente imagens usando um modelo de detecção em imagens YOLO.
 
@@ -16,6 +18,8 @@ def auto_annotate(data, det_model="yolov8x.pt", device="", output_dir=None):
         det_model (str): Caminho ou nome do modelo YOLO pre-treinado.
         device (str): Dispositivo que vai processar o modelo (e.g., 'cpu', 'cuda', '0').
         output_dir (str | None): Diretorio para salvar os resultados anotados. Se for None, um diretorio padrao sera criado.
+        desired_class_id (int | None): ID da classe desejada para anotar. Se for None, todas as classes serao anotadas.
+        draw (bool): Se True, desenha as bounding boxes na imagem original e salva a imagem anotada.
 
     Exemplos:
         >>> from AutoAnotador.annotator import auto_annotate
@@ -40,14 +44,49 @@ def auto_annotate(data, det_model="yolov8x.pt", device="", output_dir=None):
     # Escreve as bounding boxes das imagens em arquivos de texto
     for result in det_results:
         class_ids = result.boxes.cls.int().tolist()  # noqa
+        if draw == True:
+            img = cv2.imread(result.path)
+
         if len(class_ids):
             boxes = result.boxes.xywhn  # Bounding Boxes de uma imagem em formato xywh normalizado
 
-            with open(f"{Path(output_dir) / Path(result.path).stem}.txt", "w") as f:
-                # Escreve cada bounding box em uma nova linha do arquivo de texto
-                for i in range(len(boxes)):
-                    b = boxes[i]
-                    if len(b) == 0:
-                        continue
-                    box = map(str, boxes[i].reshape(-1).tolist())
-                    f.write(f"{class_ids[i]} " + " ".join(box) + "\n")
+            filtered_boxes = []
+            filtered_class_ids = []
+
+            # Filtra as bounding boxes pela class_id desejada se desired_class_id não for None
+            for i in range(len(class_ids)):
+                if desired_class_id is None or class_ids[i] == desired_class_id:
+                    filtered_boxes.append(boxes[i])
+                    filtered_class_ids.append(class_ids[i])
+
+                    if draw == True:
+                        box = boxes[i]
+                        x_center, y_center, width, height = box
+                        x_center *= img.shape[1]
+                        y_center *= img.shape[0]
+                        width *= img.shape[1]
+                        height *= img.shape[0]
+
+                        x1 = int(x_center - width / 2)
+                        y1 = int(y_center - height / 2)
+                        x2 = int(x_center + width / 2)
+                        y2 = int(y_center + height / 2)
+
+                        # Desenha a bounding box
+                        img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        # Desenha o ID da classe
+                        img = cv2.putText(img, str(class_ids[i]), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            if len(filtered_boxes) > 0:
+                with open(f"{Path(output_dir) / Path(result.path).stem}.txt", "w") as f:
+                    # Escreve cada bounding box filtrada em uma nova linha do arquivo de texto
+                    for i in range(len(filtered_boxes)):
+                        b = filtered_boxes[i]
+                        if len(b) == 0:
+                            continue
+                        box = map(str, filtered_boxes[i].reshape(-1).tolist())
+                        f.write(f"{filtered_class_ids[i]} " + " ".join(box) + "\n")
+                    if draw == True:
+                        output_image_path = Path(output_dir) / f"{Path(result.path).stem}_annotated.jpg"
+                        print("Salvando imagem anotada no diretorio " + str(output_image_path)) 
+                        cv2.imwrite(str(output_image_path), img)
